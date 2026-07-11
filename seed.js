3f05d5486221a5496c51f9d66ab33cb01188b1ed
Soulhae <herrera.benjamin@hotmail.com>
2026-07-11 03:01:27 -0400

    maxAppId porsia

--- seed.js
+++ seed.js
@@ -9,9 +9,19 @@ const steamApiKey = process.env.STEAM_WEBAPI_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-let rawGames = fs.readFileSync('steam_games_final.json', 'utf-8'); // archivo _final para tener el último juego q se agregó (los coming_soon no se agregan aún, pero en un futuro cuando tengan release date sí se agregarán). desde aquí se sigue
-let parsedGames = JSON.parse(rawGames);
-let lastAppId = parsedGames[parsedGames.length - 1].appid; // esta es la última appId que está en _final y por tanto el último juego q se ingresará a la bd
+let maxAppId = 0;
+
+let rawGames = fs.readFileSync('steam_games_final.jsonl', 'utf-8'); // archivo _final para tener el último juego q se agregó (los coming_soon no se agregan aún, pero en un futuro cuando tengan release date sí se agregarán). desde aquí se sigue
+let parsedGames = rawGames.split('\n').filter(line => line.trim() !== '').map(line => JSON.parse(line));
+for (const game of parsedGames) {
+	if (game.steam_appid > maxAppId) {
+		maxAppId = game.steam_appid;
+	}
+}
+
+let lastAppId = parsedGames[parsedGames.length - 1].steam_appid; // esta es la última appId que está en _final y por tanto el último juego q se ingresará a la bd
+// maxAppId es la appId más alta que se ha encontrado en _final, y lastAppId es la última appId que se ingresó a _final. En teoría deberían ser iguales, pero por si acaso se hace esta distinción.
+
 
 async function getSteamGames() {
 	let haveMoreResults = true;
@@ -38,7 +48,9 @@ async function main() {
 	// console.log(data);
 	// fs.writeFileSync('steam_games_2025.json', JSON.stringify(games, null, 2));
 	// console.log(lastAppId);
-	gameCount('steam_games.json');
+	// gameCount('steam_games.json');
+	console.log(lastAppId);
+	console.log(maxAppId);
 }
 
 main();
